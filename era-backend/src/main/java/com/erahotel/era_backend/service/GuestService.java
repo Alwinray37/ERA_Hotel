b768eb4ffbe046bd20364192fa241f19aef49e3b
Ryley Panopio <ryleyp529@msn.com>
2025-08-07 15:24:04 -0700

    Everything I've done for Guest up to this point

--- era-backend/src/main/java/com/erahotel/era_backend/service/GuestService.java
+++ era-backend/src/main/java/com/erahotel/era_backend/service/GuestService.java
@@ -2,6 +2,12 @@ package com.erahotel.era_backend.service;
 
 import com.erahotel.era_backend.dto.GuestDto;
 
+import java.util.List;
+
 public interface GuestService {
     GuestDto createGuest(GuestDto guestDto);
+
+    GuestDto getGuestById (Long guestId);
+
+    List<GuestDto> getAllGuests();
 }
--- era-backend/src/main/java/com/erahotel/era_backend/service/impl/GuestServiceImpl.java
+++ era-backend/src/main/java/com/erahotel/era_backend/service/impl/GuestServiceImpl.java
@@ -2,11 +2,15 @@ package com.erahotel.era_backend.service.impl;
 
 import com.erahotel.era_backend.dto.GuestDto;
 import com.erahotel.era_backend.entity.Guest;
+import com.erahotel.era_backend.exception.ResourceNotFoundException;
 import com.erahotel.era_backend.mapper.GuestMapper;
 import com.erahotel.era_backend.repository.GuestRepository;
 import com.erahotel.era_backend.service.GuestService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -20,5 +24,21 @@ public class GuestServiceImpl implements GuestService {
         Guest guest = GuestMapper.mapToGuest(guestDto);
         Guest savedGuest = guestRepository.save(guest);
         return GuestMapper.maptoGuestDto(savedGuest);
+    }
+
+    @Override
+    public GuestDto getGuestById(Long guestId) {
+        Guest guest = guestRepository.findById(guestId)
+                .orElseThrow(()->
+                        new ResourceNotFoundException("Guest is not found with given id : " + guestId));
+
+        return GuestMapper.maptoGuestDto(guest);
+    }
+
+    @Override
+    public List<GuestDto> getAllGuests() {
+       List<Guest> guests = guestRepository.findAll();
+        return guests.stream().map((guest) -> GuestMapper.maptoGuestDto(guest))
+                .collect(Collectors.toList());
     }
 }
