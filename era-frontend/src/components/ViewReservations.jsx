ff40f6e43161edde1a270f547e615575da5bb719
Ryley Panopio <ryleyp529@msn.com>
2025-08-11 18:57:53 -0700

    viewRes can now check for guest email from db

--- era-frontend/src/components/ViewReservations.jsx
+++ era-frontend/src/components/ViewReservations.jsx
@@ -1,42 +1,45 @@
-// this is where the guests can view their reservations
-
+// this is where the guests may view their reservations
+import React, {useState} from 'react';
+import { getGuestByEmail } from '../service/GuestService';
 export default function ViewReservations() {
 
-    const dummyData = [
-        {
-            "id": 1,
-            "email": "john@email.com",
-            "reservationNumber": "res10001"
-        },
-        {
-                "id": 2,
-                "email": "tomK@email.com",
-                "reservationNumber": "res10002"
-        },
+    //stuff to form the booking forms
+    const [formData, setFormData] = useState({
+        email: "",
+        reservationNumber: ""
+    });
 
-           {
-                    "id": 3,
-                    "email": "tomK@email.com",
-                    "reservationNumber": "res10002"
-           }
+    // handle form submission
+    const handleSubmit = (e) => {
+       e.preventDefault();
 
-    ];
-    function handleSubmit (){
+        // system checks if the email is in the database already
+        getGuestByEmail(formData.email)
+        .then(guest => {
+            if (guest) {
+                console.log('Guest already exists:', guest);
+                // Proceeds with booking using the existing guest data
+            }
+        })
+        .catch (error => {
+            console.error('Error fetching guest:', error);
+        });
+    }
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
     }
 
     return (
         <div className="main-content">
             <form onSubmit = {handleSubmit} >
                 <h3>View Reservations</h3>
-                <input placeholder="Enter Email" />
+                <input placeholder="Enter Email" type="email" name="email" value={formData.email} onChange={handleChange}/>
                 <input placeholder="Enter Reservation Number" />
                  <button className='btn btn-primary mb-2' type="submit">Submit</button>
             </form>
-
-
         </div>
 
     )
-
 }
