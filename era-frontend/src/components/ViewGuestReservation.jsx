229c389ea8f970407468e1cfc4d1c8bd2331994b
alwinray37 <alwinray37@gmail.com>
2025-08-13 15:52:27 -0700

    created cancel reservation functionality

--- era-frontend/src/components/ViewGuestReservation.jsx
+++ era-frontend/src/components/ViewGuestReservation.jsx
@@ -1,5 +1,5 @@
 import { Navigate, useLocation } from "react-router-dom"
-import { getReservationById } from "../service/ReservationService";
+import { getReservationById, updateReservation } from "../service/ReservationService";
 import { useState, useEffect } from "react";
 import { getRoomByNumber } from "../service/RoomService";
 import { getGuestByEmail } from "../service/GuestService";
@@ -20,6 +20,7 @@ export default function ViewGuestRes(){
     const [room, setRoom] = useState(null);
     const [guest, setGuest] = useState(null);
 
+    // get reservation obj from database, save it to reservation var^^
     useEffect(() => {
         const fetchReservation = async () => {
             try {
@@ -32,7 +33,7 @@ export default function ViewGuestRes(){
         };
         if (resId) fetchReservation();
     }, [resId]);
-
+    // get room obj from database and save it to room var
     useEffect( () => {
         const fetchRoom = async () => {
             console.log(reservation.roomNumber)
@@ -46,7 +47,7 @@ export default function ViewGuestRes(){
         };
         if(reservation) fetchRoom();
     }, [reservation]);
-
+    // get guest obj from database and save it to guest var
     useEffect( () => {
         const fetchGuest = async () => {
             try {
@@ -58,7 +59,16 @@ export default function ViewGuestRes(){
             }
         };
         if(reservation) fetchGuest();
-    }, [reservation])
+    }, [reservation]);
+
+    const handleCancel = () => {
+        const updRes = {...reservation, status : "Cancelled" };
+        console.log("Reservation status: " ,reservation.status);
+        setReservation(updRes); // triggers re-render of component 
+        // update reservation in the backend
+        updateReservation(reservation.reservationId, reservation);
+        
+    }
 
 
     // loading message before data is loaded
@@ -69,22 +79,41 @@ export default function ViewGuestRes(){
             </div>
         )
     }
-
+    // if reservation is cancelled, show a custom page
+    if(reservation.status == "Cancelled"){
+        return(
+            <div className="content d-flex flex-column align-items-center p-4">
+                <h2>Reservation Cancelled</h2>
+                <div className="res-content container shadow p-4">
+                    <p><strong>Name:</strong> {guest.name}</p>
+                    <p><strong>Email:</strong> {reservation.guestEmail}</p>
+                    <p><strong>Room Number:</strong> {reservation.roomNumber}</p>
+                    <p><strong>Room Details:</strong> {room.description}</p>
+                    <p><strong>Check-in:</strong> {new Date(reservation.startDate).toDateString()}</p>
+                    <p><strong>Check-out:</strong> {new Date(reservation.endDate).toDateString()}</p>
+                    <p><strong>Status:</strong> {reservation.status}</p>
+                </div>
+            </div>
+            
+        )
+    }
+    // actual data to view when objects are not null 
     return(
         <div className="content">
             <div className="container  shadow p-4 ">
                 <h2>Guest Reservation Details</h2>
-            <p>Name: {guest.name}</p>
-            <p>Email: {reservation.guestEmail}</p>
-            <p>Room Number: {reservation.roomNumber}</p>
-            <p>Room Details: {room.description}</p>
-            <p>Check-in: {new Date(reservation.startDate).toDateString()}</p>
-            <p>Check-out: {new Date(reservation.endDate).toDateString()}</p>
+                <p><strong>Name:</strong> {guest.name}</p>
+                <p><strong>Email:</strong> {reservation.guestEmail}</p>
+                <p><strong>Room Number:</strong> {reservation.roomNumber}</p>
+                <p><strong>Room Details:</strong> {room.description}</p>
+                <p><strong>Check-in:</strong> {new Date(reservation.startDate).toDateString()}</p>
+                <p><strong>Check-out:</strong> {new Date(reservation.endDate).toDateString()}</p>
+                <p><strong>Status:</strong> {reservation.status}</p>
 
-            <div className="button-container contianer d-flex gap-4 justify-content-start w-2">
-                <button className="btn btn-warning">Modify Reservation</button>
-                <button className="btn btn-danger">Cancel Reservation</button>
-            </div>
+                <div className="button-container contianer d-flex gap-4 justify-content-start w-2">
+                    <button className="btn btn-warning">Modify Reservation</button>
+                    <button className="btn btn-danger" onClick={handleCancel}>Cancel Reservation</button>
+                </div>
             </div>
         </div>
     )
