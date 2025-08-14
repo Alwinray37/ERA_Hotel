0e5a06fd1078d1222f03bd7c532a79051f8994c9
FarshadA786 <farshad.azam.908@my.csun.edu>
2025-08-13 17:27:05 -0700

    dashboard

--- era-frontend/src/components/AdminDashboard.jsx
+++ era-frontend/src/components/AdminDashboard.jsx
@@ -1,16 +1,91 @@
-export default function AdminDashboard() {
-  const admin = JSON.parse(localStorage.getItem("admin") || "{}");
+import { Navigate, useLocation } from "react-router-dom"
+import { getReservationById } from "../service/AdminService";
+import { useState, useEffect } from "react";
+import { getRoomByNumber } from "../service/AdminService";
+import { getGuestByEmail } from "../service/AdminService";
 
-  const logout = () => {
-    localStorage.removeItem("admin");
-    window.location.href = "/admin-login";
-  };
 
-  return (
-    <div style={{ maxWidth: 720, margin: "2rem auto" }}>
-      <h2>Admin Dashboard</h2>
-      <p>Welcome, {admin?.name} ({admin?.email})</p>
-      <button onClick={logout}>Logout</button>
-    </div>
-  );
+export default function ViewAdminRes(){
+    const location = useLocation();
+
+    // takes the guest email and resID from AdminDashboard page
+    const email = location.state.email;
+    const resId = location.state.resID; // reservationID (ABC123)
+    // checking if values transfered
+    // console.log("Guest Email", email);
+    // console.log("Reservation Number", resId);
+
+    // get reservation by id, room by roomNumber
+    const [reservation, setReservation] = useState(null);
+    const [room, setRoom] = useState(null);
+    const [guest, setGuest] = useState(null);
+
+    useEffect(() => {
+        const fetchReservation = async () => {
+            try {
+                const res = await getReservationById(resId);
+                setReservation(res);
+                console.log("res obj: ", res);
+            } catch (error) {
+                console.error("Error fetching admin:", error);
+            }
+        };
+        if (resId) fetchReservation();
+    }, [resId]);
+
+    useEffect( () => {
+        const fetchRoom = async () => {
+            console.log(reservation.roomNumber)
+            try {
+                const res = await getRoomByNumber(reservation.roomNumber);
+                setRoom(res);
+                console.log("room: " , room);
+            } catch {
+                console.log("Error fetching room");
+            }
+        };
+        if(reservation) fetchRoom();
+    }, [reservation]);
+
+    useEffect( () => {
+        const fetchGuest = async () => {
+            try {
+                const res = await getAdmiByEmail(email);
+                setGuest(res);
+                console.log("guest: " , room);
+            } catch {
+                console.log("Error fetching room");
+            }
+        };
+        if(reservation) fetchGuest();
+    }, [reservation]);
+
+
+    // loading message before data is loaded
+    if(!reservation || !room || !guest){
+        return (
+            <div className="main-content container">
+                loading
+            </div>
+        )
+    }
+
+    return(
+        <div className="content">
+            <div className="container shadow p-4 ">
+                <h2>Guest Reservation Details</h2>
+            <p>Name: {guest.name}</p>
+            <p>Email: {reservation.guestEmail}</p>
+            <p>Room Number: {reservation.roomNumber}</p>
+            <p>Room Details: {room.description}</p>
+            <p>Check-in: {new Date(reservation.startDate).toDateString()}</p>
+            <p>Check-out: {new Date(reservation.endDate).toDateString()}</p>
+
+            <div className="button-container contianer d-flex gap-4 justify-content-start w-2">
+                <button className="btn btn-warning">Modify Reservation</button>
+                <button className="btn btn-danger">Cancel Reservation</button>
+            </div>
+            </div>
+        </div>
+    )
 }
