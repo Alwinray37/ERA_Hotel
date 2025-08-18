5173f4798d825b8c9e48da4759320c3d34f01aba
Ryley Panopio <ryleyp529@msn.com>
2025-08-17 18:58:12 -0700

    The Junit testing I've been trying to do (pretty sure it's wrong tbh) at this time for the GuestServiceImplTest

--- era-backend/src/test/java/com/erahotel/era_backend/service/impl/GuestServiceImplTest2.java
+++ era-backend/src/test/java/com/erahotel/era_backend/service/impl/GuestServiceImplTest2.java
@@ -3,20 +3,42 @@ package com.erahotel.era_backend.service.impl;
 import com.erahotel.era_backend.entity.Guest;
 import com.erahotel.era_backend.repository.GuestRepository;
 import com.erahotel.era_backend.service.GuestService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith((MockitoExtension.class))
+
+
 
 public class GuestServiceImplTest2 {
     @Mock
+    GuestRepository guestRepository;
+
+    @InjectMocks Mock
     GuestService guestService;
 
     @Test
     void createGuest() {
         System.out.println(("Create guest");
         Guest guest = new Guest();
-        guestService.createGuest(guest);
+        guest.setGuestId(1);
+        guest.setName("John Doe");
+        guest.setEmail("john@email.com");
+        guest.getPhone("888-888-8888");
+        Mockito.when(guestRepository.save(guest)).thenReturn(guest);
+        Guest createGuest = guestService.createGuest(guest);
+        //Test guest = matched guest
+        Assertions.assertNotNull(createGuest));
+        Assertions.assertEquals(guest.getGuestId(), createGuest.getGuestId());
+        Assertions.assertEquals(guest.getName(), createGuest.getName());
+        Assertions.assertEquals(guest.getEmail(), createGuest.getEmail());
+        Assertions.assertEquals(guest.getPhone(), createGuest.getPhone());
+        Assertions.assertTrue(guest.getGuestId()==1);
     }
 
     @Test
