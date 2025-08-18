1d1b71e5bc1810930c1be3607fd963fbd1db415c
Ryley Panopio <ryleyp529@msn.com>
2025-08-17 19:14:11 -0700

    Updating junit stuff for my branch

---
+++ era-backend/src/test/java/com/erahotel/era_backend/service/impl/ReservationServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.erahotel.era_backend.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationServiceImplTest {
+
+    @Test
+    void createReservation() {
+    }
+
+    @Test
+    void getReservationById() {
+    }
+
+    @Test
+    void getAllReservations() {
+    }
+
+    @Test
+    void deleteReservation() {
+    }
+
+    @Test
+    void updateReservation() {
+    }
+}
