1027d90e625dab25ece3d672d1e9cac5ae2f7a14
alwinray37 <alwinray37@gmail.com>
2025-08-17 17:46:23 -0700

    final

--- era-backend/src/test/java/com/erahotel/era_backend/controller/ReservationControllerTest.java
+++ era-backend/src/test/java/com/erahotel/era_backend/controller/ReservationControllerTest.java
@@ -1,4 +1,28 @@
+package com.erahotel.era_backend.controller;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ReservationControllerTest {
-  
+
+    @Test
+    void createReservation() {
+    }
+
+    @Test
+    void getReservationById() {
+    }
+
+    @Test
+    void getAllReservations() {
+    }
+
+    @Test
+    void deleteReservation() {
+    }
+
+    @Test
+    void updateReservation() {
+    }
 }
--- era-backend/src/test/java/com/erahotel/era_backend/service/impl/GuestServiceImplTest.java
+++ era-backend/src/test/java/com/erahotel/era_backend/service/impl/GuestServiceImplTest.java
@@ -2,8 +2,6 @@ package com.erahotel.era_backend.service.impl;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GuestServiceImplTest {
 
     @Test
